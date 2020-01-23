6fc6015c79cacca1a5d82bdb29b0d7bf65127b85
gandh99 <gandh99@hotmail.com>
2020-01-23 21:32:41 +0800

    Added a function in animationbuttongroup that will first clear the board of visited cells when the play button is clicked

--- scripts/buttongroup/animationbuttongroup.js
+++ scripts/buttongroup/animationbuttongroup.js
@@ -1,4 +1,5 @@
 import Dijkstra from "../algorithms/dijkstra.js";
+import CellState from "../cellstate.js";
 
 export default class AnimationButtonGroup {
     constructor(grid, utilityBar) {
@@ -15,8 +16,24 @@ export default class AnimationButtonGroup {
     initFunctionality() {
         this.playButton.addEventListener("click", () => {
             this.utilityBar.sendAnimationPlayEvent();
+            this.clearVisitedCells();
             this.startAnimation();
         });
+    }
+
+    // Clear the board of all cells except source, destination and obstacles
+    clearVisitedCells() {
+        let cellModelMatrix = this.grid.getCellModelMatrix;
+        for (let i = 0; i < this.grid.getNumberOfRows; i++) {
+            for (let j = 0; j < this.grid.getNumberOfCols; j++) {
+                if (cellModelMatrix[i][j].getCellState == CellState.SOURCE
+                    || cellModelMatrix[i][j].getCellState == CellState.DESTINATION
+                    || cellModelMatrix[i][j].getCellState == CellState.OBSTACLE) {
+                    continue;
+                }
+                cellModelMatrix[i][j].updateCellState(CellState.BLANK);
+            }
+        }
     }
 
     startAnimation() {
