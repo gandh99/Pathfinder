5b3399df9a546f441d0eefad460af8e7951da9ca
gandh99 <gandh99@hotmail.com>
2020-01-27 22:33:17 +0800

    Finished adding the content for the help window

--- scripts/button/helpbutton.js
+++ scripts/button/helpbutton.js
@@ -1,9 +1,15 @@
 export default class HelpButton {
     constructor() {
+        let modalWindow = document.getElementById("help-window-modal");
+        
         let helpButton = document.getElementById("help-button");
         helpButton.addEventListener("click", () => {
-            let modalWindow = document.getElementById("help-window-modal");
             modalWindow.style.display = "block";
-        })
+        });
+
+        let doneButton = document.getElementById("close-help-window");
+        doneButton.addEventListener("click", () => {
+            modalWindow.style.display = "none";
+        });
     }
 }
